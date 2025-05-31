ec7b346c84e058f7fc80052a1c4f2a66871f5ef9
jerrychen <782323370@qq.com>
2025-05-31 21:08:30 +0800

    delete ApplicationTest.java

--- src/main/java/com/jerry/rtfd/config/AwsConfig.java
+++ src/main/java/com/jerry/rtfd/config/AwsConfig.java
@@ -1,7 +1,6 @@
 package com.jerry.rtfd.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;

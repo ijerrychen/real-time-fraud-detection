444b3266b1313ef85f611d3e914c3dc47ab40c8a
jerrychen <782323370@qq.com>
2025-06-02 10:47:12 +0800

    优化视频播放

--- src/main/java/com/jerry/rtfd/Application.java
+++ src/main/java/com/jerry/rtfd/Application.java
@@ -2,14 +2,8 @@ package com.jerry.rtfd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableAsync;
-
-import java.util.concurrent.Executor;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @EnableAsync
